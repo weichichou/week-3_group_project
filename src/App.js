84f489b524defbb685cc50b1ef1debafd265a200
weichichou <weichi.cc@gmail.com>
2019-10-21 17:09:19 +0200

    Add inital html, change styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {/*  <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Fresh Tomatoes
+        </h1>
       </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+<h6>Made with ♥ at Codaisseur</h6>
+
+      </footer>
     </div>
   );
 }
