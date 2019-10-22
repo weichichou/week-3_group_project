92b9a513dbdd94923ec7620e5ddd9a57380ba953
weichichou <weichi.cc@gmail.com>
2019-10-22 17:38:49 +0200

    add like button

--- src/component/Review.js
+++ src/component/Review.js
@@ -5,13 +5,25 @@ import Api from './Api';
 
 export default class Review extends Component {
   
+    state = {
+        numLikes: 0
+    };
+
+    increment = () => {
+        this.setState({
+            numLikes: this.state.numLikes + 1
+        })
+    };
 
     render (){
         return (
             <div>
-                {/* <p> { this.props.reviewTexts}</p>
-                <h4>Score: { this.props.score }</h4> */}
+                <p>This movie has <b>{ this.state.numLikes}</b> likes!  
+                    <button onClick={this.increment}>Like</button></p>
+               
+                <p></p>
                 <h4>Score: {this.props.score} </h4>
+                
             </div>
         )
     }
