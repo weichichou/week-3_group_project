916f3f47915fdeeb4b1dc976985789064df05d6b
weichichou <weichi.cc@gmail.com>
2019-10-22 16:49:46 +0200

    fetch data from movie api

--- src/component/Review.js
+++ src/component/Review.js
@@ -3,11 +3,37 @@ import { directive } from '@babel/types';
 import './Review.css';
 
 export default class Review extends Component {
+  
+  state = {
+      title: '',
+      score: [],
+  }
+
+  componentDidMount() {
+    fetch('https://my-json-server.typicode.com/LavanyaJay/movieapi/movies')
+    .then(response => response.json())
+    .then((data)=>{
+        //console.log(data[0].title)
+        this.setState({
+             title: data[0].title
+        })
+        console.log(this.state.title)
+
+        /* 
+        console.log(data)
+        this.setState({
+            title: data
+        }) */
+    })
+}
+
+
     render (){
         return (
             <div>
-                <p> { this.props.reviewTexts}</p>
-                <h4>Score: { this.props.score }</h4>
+                {/* <p> { this.props.reviewTexts}</p>
+                <h4>Score: { this.props.score }</h4> */}
+                <h4>Title: { this.state.title }</h4>
             </div>
         )
     }
